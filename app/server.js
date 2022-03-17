caa2bf21ed9217e50d12db365656f8548b6ef3bf
Adrien Soursou <adrien.soursou@ens.uvsq.fr>
2022-03-17 17:29:08 +0100

    Fix

--- app/server.js
+++ app/server.js
@@ -12,5 +12,17 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.get('/ping', (req, res) => {
+  res.send('pong');
+});
+
+app.get('/test', (req, res) => {
+  res.send('plop');
+});
+
+app.get('users/profile', (req, res) => {
+  res.send('profile');
+});
+
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
