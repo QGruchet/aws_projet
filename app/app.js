205a43bd5659741e7fbfa1ad79b7c93f3700a47c
uvsq21806570 <matteo.giraudo@ens.uvsq.fr>
2022-04-14 16:21:00 +0200

    Communicating canvas with socket.io

--- app/app.js
+++ app/app.js
@@ -1,8 +1,11 @@
+/*const http = require('http');
 const express = require('express');
 const path = require('path');
 
 // Application configuration
 const app = express();
+const server = http.createServer();
+const io = require('socket.io')(server);
 const port = process.env.PORT;
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -13,8 +16,55 @@ var gameRouter = require('./routes/game');
 app.use('/game', gameRouter);
 
 // Start listening to requests
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+server.on('listening', () => {
+  console.log('Listening on port ' + port)
 });
+
+server.listen(port);
+
+io.sockets.on('connection', (socket) => {
+  console.log('Client connected: ' + socket.id)
+  socket.on('draw', (data) => socket.broadcast.emit('draw', data))
+  socket.on('disconnect', () => console.log('Client disconnected'))
+})
+
+module.exports = app;*/
+
+const express = require('express');
+const path = require('path');
+
+// Application configuration
+const app = express();
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+const port = process.env.PORT;
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes configuration
+var indexRouter = require('./routes/index');
+app.use('/', indexRouter);
+var gameRouter = require('./routes/game');
+app.use('/game', gameRouter);
+
+// Start listening to requests
+server.on('listening', () => {
+  console.log('Listening on port ' + port)
+});
+
+server.listen(port);
+
+io.sockets.on('connection', (socket) => {
+  console.log('Client connected: ' + socket.id)
+  socket.on('draw', (data) => socket.broadcast.emit('draw', data))
+  socket.on('pencil', (data) => socket.broadcast.emit('pencil', data))
+  socket.on('rubber', (data) => socket.broadcast.emit('rubber', data))
+  socket.on('color', (color) => socket.broadcast.emit('color', color))
+  socket.on('lineWidth', (data) => socket.broadcast.emit('lineWidth', data))
+  socket.on('opacity', (data) => socket.broadcast.emit('opacity', data))
+  socket.on('clear', () => socket.broadcast.emit('clear'))
+  socket.on('fill', (color) => socket.broadcast.emit('fill', color))
+  socket.on('undo', (points) => socket.broadcast.emit('undo', points))
+  socket.on('disconnect', () => console.log('Client disconnected'))
+})
 
 module.exports = app;
--- app/public/js/game.js
+++ app/public/js/game.js
@@ -1,3 +1,5 @@
+const socket = io('http://localhost:80');
+
 /**
  * A class who representing a drawing
  */
@@ -14,7 +16,7 @@ class Drawing {
         this.prevColor = null;  // Color of the line
         this.prevOpacity = null;    // Opacity of the line
         this.prevLineWidth = null;  //Width of the line
-        this.points=[];
+        this.points = [];
 
 
         this.canvas = document.querySelector(canvas);
@@ -35,14 +37,14 @@ class Drawing {
             this.prevY = (e.clientY - this.canvas.offsetTop) * this.canvas.height / this.canvas.clientHeight;
 
             // Save the line point by point
-            this.points.push({x:this.prevX, y: this.prevY, size: this.prevLineWidth, color:this.prevColor, mode:"begin"})
+            this.points.push({ x: this.prevX, y: this.prevY, size: this.prevLineWidth, color: this.prevColor, mode: "begin" })
 
         })
 
         // Disable drawing if users stop holding mouse
         this.canvas.addEventListener("mouseup", () => {
             this.dessin = false;
-            this.points.push({x:this.prevX, y: this.prevY, size: this.prevLineWidth, color:this.prevColor, mode:"end"})
+            this.points.push({ x: this.prevX, y: this.prevY, size: this.prevLineWidth, color: this.prevColor, mode: "end" })
         })
 
         // Disable drawing if the pointer is out of the canvas
@@ -52,23 +54,28 @@ class Drawing {
 
         // Begin line when the user move his mouse.
         this.canvas.addEventListener("mousemove", (e) => {
-        if(this.dessin === true) {
-            // Set position of the pointer
-            let currX = (e.clientX - this.canvas.offsetLeft) * this.canvas.width / this.canvas.clientWidth;
-            let currY = (e.clientY - this.canvas.offsetTop) * this.canvas.height / this.canvas.clientHeight;
+            if (this.dessin === true) {
+                // Set position of the pointer
+                let currX = (e.clientX - this.canvas.offsetLeft) * this.canvas.width / this.canvas.clientWidth;
+                let currY = (e.clientY - this.canvas.offsetTop) * this.canvas.height / this.canvas.clientHeight;
+                var data = { x1: this.prevX, y1: this.prevY, x2: currX, y2: currY }
+                // Begin drawing
+                this.myDraw(this.prevX, this.prevY, currX, currY)
+                socket.emit("draw", data)
+                this.prevX = currX
+                this.prevY = currY
 
-            // Begin drawing
-            this.context.beginPath();
-            this.context.moveTo(this.prevX, this.prevY);
-            this.context.lineTo(currX, currY)
-            this.context.closePath();
-            this.context.stroke()
-            this.prevX = currX
-            this.prevY = currY
-
-            this.points.push({x:this.prevX, y: this.prevY, size: this.prevLineWidth, color:this.prevColor, mode:"draw"})
-        }
+                this.points.push({ x: this.prevX, y: this.prevY, size: this.prevLineWidth, color: this.prevColor, mode: "draw" })
+            }
         })
+    }
+
+    myDraw(x1, y1, x2, y2) {
+        this.context.beginPath();
+        this.context.moveTo(x1, y1);
+        this.context.lineTo(x2, y2)
+        this.context.closePath();
+        this.context.stroke()
     }
 
     /**
@@ -77,6 +84,7 @@ class Drawing {
     erase() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
         this.points = []
+        socket.emit('clear')
     }
 
     /**
@@ -86,6 +94,8 @@ class Drawing {
         this.context.strokeStyle = "white"
         this.context.globalAlpha = this.prevOpacity
         this.context.lineWidth = this.prevLineWidth
+        var data = { opacity: this.prevOpacity, width: this.prevLineWidth }
+        socket.emit('rubber', data)
     }
 
     /**
@@ -95,6 +105,8 @@ class Drawing {
         this.context.globalAlpha = this.prevOpacity
         this.context.strokeStyle = this.prevColor
         this.context.lineWidth = this.prevLineWidth
+        var data = {opacity: this.prevOpacity, color: this.prevColor, width: this.prevLineWidth}
+        socket.emit('pencil', data)
     }
 
     /**
@@ -120,6 +132,7 @@ class Drawing {
         colorPicker.addEventListener('change', (e) => {
             this.context.strokeStyle = e.target.value;
             this.prevColor = e.target.value;
+            socket.emit('color', e.target.value)
         })
     }
 
@@ -136,6 +149,8 @@ class Drawing {
             label.innerHTML = width;
             this.context.lineWidth = width / 10;
             this.prevLineWidth = e.target.value / 10;
+            var data = { ctx_width: this.context.lineWidth, prev_width: this.prevLineWidth }
+            socket.emit("lineWidth", data)
         })
     }
 
@@ -151,6 +166,8 @@ class Drawing {
             label.innerHTML = opacity;
             this.context.globalAlpha = opacity / 100;
             this.prevOpacity = e.target.value / 100;
+            var data = { alpha: this.context.globalAlpha, opacity: this.prevOpacity }
+            socket.emit("opacity", data)
         })
     }
 
@@ -162,42 +179,44 @@ class Drawing {
         this.context.rect(0, 0, this.canvas.width, this.canvas.height)
         this.context.fillStyle = this.context.strokeStyle
         this.context.fill()
+        socket.emit('fill', this.context.fillStyle)
     }
 
     /**
      * Function use to undo the line point by point
      */
-    undoLast(){
+    undoLast() {
         this.points.pop();
+        socket.emit('undo', this.points)
         this.redrawAll();
     }
 
     /**
      * Function use to redraw all the canvas except the point that we just undo
      */
-    redrawAll(){
-        if(this.points.length === 0){
+    redrawAll() {
+        if (this.points.length === 0) {
             return;
         }
-        this.context.clearRect(0,0, this.canvas.width, this.canvas.height);
-        for(let i = 0; i < this.points.length; i++) {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        for (let i = 0; i < this.points.length; i++) {
             const pt = this.points[i];
             let begin = false;
-            if(this.context.lineWidth !== pt.size) {
+            if (this.context.lineWidth !== pt.size) {
                 this.context.lineWidth = pt.size;
-                begin=true;
+                begin = true;
             }
-            if(this.context.strokeStyle !== pt.color) {
+            if (this.context.strokeStyle !== pt.color) {
                 this.context.strokeStyle = pt.color;
                 this.prevColor = pt.color;
-                begin=true;
+                begin = true;
             }
-            if(pt.mode === "begin" || begin){
+            if (pt.mode === "begin" || begin) {
                 this.context.beginPath();
-                this.context.moveTo(pt.x,pt.y);
+                this.context.moveTo(pt.x, pt.y);
             }
             this.context.lineTo(pt.x, pt.y);
-            if(pt.mode === "end" || (i === this.points.length-1)) {
+            if (pt.mode === "end" || (i === this.points.length - 1)) {
                 this.context.stroke();
             }
         }
@@ -210,13 +229,13 @@ class Drawing {
  */
 window.onload = () => {
 
-    let canvas = new Drawing('#DrawingArea')
+    let cvs = new Drawing('#DrawingArea')
 
-    canvas.draw()
+    cvs.draw()
 
     // User click on "clear" button
     document.querySelector("#reset").addEventListener("click", () => {
-        canvas.erase()
+        cvs.erase()
     })
 
     // User click on "save" button
@@ -232,39 +251,72 @@ window.onload = () => {
     document.getElementById("rubber").addEventListener("click", () => {
         // Change the cursor to make sure user doing what he wants
         document.body.style.cursor = "url(\"../assets/rubber.cur\") 0 20, auto"
-        canvas.rubber()
+        cvs.rubber()
     })
 
     // User click on "pencil" button
     document.getElementById("pencil").addEventListener("click", () => {
         // Change the cursor to make sure user doing what he wants
         document.body.style.cursor = "url(\"../assets/pencil.cur\") 0 22, auto"
-        canvas.draw()
+        cvs.draw()
     })
 
     // User click on color picker button
     document.getElementById("colorPicker").addEventListener("click", () => {
-        canvas.changeColor()
+        cvs.changeColor()
     })
 
     // User click on "Line width" button
     document.getElementById("rangeLineWidth").addEventListener("click", () => {
-        canvas.changeLineWidth()
+        cvs.changeLineWidth()
     })
 
     // User click on "Opacity" button
     document.getElementById("rangeOpacity").addEventListener("click", () => {
-        canvas.changeOpacity()
+        cvs.changeOpacity()
     })
 
     // User click on "fill" button
     document.getElementById("fillCanva").addEventListener("click", () => {
-        canvas.fill()
+        cvs.fill()
     })
 
     // User click on "undo" button
     document.getElementById("undo").addEventListener("click", () => {
-        canvas.undoLast()
+        cvs.undoLast()
     })
 
+    socket.on('draw', (data) => cvs.myDraw(data.x1, data.y1, data.x2, data.y2))
+    socket.on('pencil', (data) => {
+        cvs.context.globalAlpha = data.opacity
+        cvs.context.strokeStyle = data.color
+        cvs.context.lineWidth = data.width
+    })
+    socket.on('rubber', (data) => {
+        cvs.context.strokeStyle = "white"
+        cvs.context.globalAlpha = data.opacity
+        cvs.context.lineWidth = data.width
+    })
+    socket.on('color', (data) => {
+        cvs.context.strokeStyle = data
+        cvs.prevColor = data
+    })
+    socket.on('lineWidth', (data) => {
+        cvs.context.lineWidth = data.ctx_width
+        cvs.prevLineWidth = data.prev_width
+    })
+    socket.on('opacity', (data) => {
+        cvs.context.globalAlpha = data.alpha
+        cvs.prevOpacity = data.opacity
+    })
+    socket.on('fill', (color) => {
+        cvs.context.beginPath()
+        cvs.context.rect(0, 0, cvs.canvas.width, cvs.canvas.height)
+        cvs.context.fillStyle = color
+        cvs.context.fill()
+    })
+    socket.on('undo', (points) => {
+        cvs.points = points
+        cvs.redrawAll()
+    })
 }
