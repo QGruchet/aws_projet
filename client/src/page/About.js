168b117e293cc43619d67a2825e71bc9cb5028a0
qgruchet <quentin.gruchet78@gmail.com>
2022-04-25 19:27:27 +0200

    Ajout des pages principales

---
+++ client/src/page/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navigation from "../components/Navigation";
+
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <h1>A propos</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Aliquam rutrum ante non justo posuere, eu ultricies risus eleifend.
+                Aenean ut sagittis eros. Nunc ac suscipit mi, a aliquet diam.
+                Etiam mollis vel purus sed volutpat. Duis non mi tortor.
+                Pellentesque volutpat felis sit amet lorem maximus,
+                nec varius elit finibus. Cras nec varius arcu.</p>
+        </div>
+    );
+};
+
+export default About;
---
+++ client/src/page/Game.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from "../logo.png"
+import "../draw.js"
+import Navigation from "../components/Navigation";
+
+const Game = () => {
+    return (
+    <div>
+        <Navigation/>
+     </div>
+    )
+};
+
+export default Game;
