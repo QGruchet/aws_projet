d89185bf10bcfad30af87749289b7038324ce0d8
qgruchet <quentin.gruchet78@gmail.com>
2022-04-25 19:28:04 +0200

    Ajout des components

---
+++ client/src/components/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from "../logo.png"
+
+const Logo = () => {
+    return (
+        <div>
+            <img src={logo} alt={"Cannot load logo"}/>
+        </div>
+    );
+};
+
+export default Logo;
